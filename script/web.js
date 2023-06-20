66861ddae1f3d467cbc041972f6ca903ac4d6e50
Leandro Ezequiel Pedercini <leandroezequielpedercini@gmail.com>
2023-06-20 01:53:01 -0300

    shfd

--- script/web.js
+++ script/web.js
@@ -1,6 +1,6 @@
 // header //
 const cabezal = document.getElementById("cabezal-index-HTML")
-cabezal.innerHTML = '<img src="header/Puerto Mopane.png" alt="Puerto Mopane Cabañas" class="puerto-mopane-cabañas"> <img src="header/Facebook.png" alt="Facebook" class="facebook" > <img src="header/instagram.png" alt="Instagram" class="instagram" > <img src="header/whatsapp.png" alt="Whatsapp" class="whatsapp" > <img src="header/Movil.png" alt="Movil" class="movil" > <img src="header/mail.png" alt="E-mial" class="e-mail" > '  
+cabezal.innerHTML = '<img src="/header/Puerto Mopane.png" alt="Puerto Mopane Cabañas" class="puerto-mopane-cabañas"> <img src="/header/Facebook.png" alt="Facebook" class="facebook" > <img src="/header/instagram.png" alt="Instagram" class="instagram" > <img src="/header/whatsapp.png" alt="Whatsapp" class="whatsapp" > <img src="/header/Movil.png" alt="Movil" class="movil" > <img src="/header/mail.png" alt="E-mial" class="e-mail" > '  
 
 // Links //
 const links = document.getElementById("links")
@@ -17,11 +17,6 @@ imgparrafo.innerHTML = '<p class="Parrafo-con-imagen "> Un lugar para disfrutar.
 // Parrafo solo //
 const parrafo = document.getElementById("parrafo")
 parrafo.innerHTML = ' <h2 class="h2-parrafo">Puerto Mopane :</h2>  <div><p class="p1-parrafo">En la exclusiva localidad de MONTE HERMOSO dentro de 5 ha. de inigualable tranquilidad, se conjugan las vistas agrestes de los médanos blancos junto al extenso y frondoso bosque del pintoresco barrio residencial “Las Dunas” .</p> </div> <div><p class="p2-parrafo">Ubicado en un lugar privilegiado, a 700 mts. de la playa y a 5 minutos del centro de la ciudad, se encuentra <h5>PUERTO MOPANE.</h3></div> <div><p class="p3-parrafo" >Descubra un atardecer inolvidable, o bien, un amanecer donde sólo reina la paz y la tranquilidad.</p> </div> <div><p class="p4-parrafo">Queremos que disfrute junto a nosotros de este lugar … de su amplia piscina y sus recreaciones como la cancha de voley y fútbol para niños y así poder transmitirles la alegría y armonía que todo esto significa.</p></div> <div><p class="p5-parrafo">Paseos, cabalgatas, pesca y diversión nocturna para chicos y grandes junto a sus extensas playas ÚNICAS con el sol saliendo y poniéndose sobre el mar hacen de MONTE HERMOSO una ciudad privilegiada y de PUERTO MOPANE” el lugar ideal para el descanso de su familia.</p>  </div>   <div><h4 class="h4-parrafo">Nuestra mayor publicidad es la recomendación de aquellos que han estado con nosotros.  Gracias por elegirnos! </h4></div> '
-
-
-
-
-
 
 // Pie de pagina //
 const imagenatardecer = document.getElementById("imagen-atardecer")
