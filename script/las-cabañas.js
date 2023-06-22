c1ed3d538b4d963c58831d49bd9723ececb14f51
Leandro Ezequiel Pedercini <leandroezequielpedercini@gmail.com>
2023-06-21 23:04:10 -0300

    Seguimos trabajando en la pagina web

---
+++ script/las-cabañas.js
@@ -0,0 +1,18 @@
+/* las-cabañas.html */
+
+// header //
+const cabezal = document.getElementById("cabezal-index-HTML")
+cabezal.innerHTML = '<img src="header/Puerto Mopane.png" alt="Puerto Mopane Cabañas" class="puerto-mopane-cabañas"> <img src="header/Facebook.png" alt="Facebook" class="facebook" > <img src="header/instagram.png" alt="Instagram" class="instagram" > <img src="header/whatsapp.png" alt="Whatsapp" class="whatsapp" > <img src="header/Movil.png" alt="Movil" class="movil" > <img src="header/mail.png" alt="E-mial" class="e-mail" > '  
+
+// Links //
+const links = document.getElementById("links")
+links.innerHTML = '<a href="index.html" class="nuestroLugar-HTML">Nuestro lugar</a> <a href="las-cabañas.html" class="lasCabañas-HTML">Las cabañas</a> <a href="ubicacion.html" class="ubicacion-HTML">Ubicacion</a><a href="contacto.html" class="contacto-HTML">Contacto</a>'
+
+
+/* Imagen */
+const imagen = document.getElementById("imagen")
+imagen.innerHTML = '<img src="carrusel/3.jpeg" alt="Paisaje" class="imagen-portada">' 
+
+/* Descripcion */
+const descripcion = document.getElementById("descripcion")
+descripcion.innerHTML = '<p>El complejo incluye una amplia piscina con deck y solarium, canchas de voley, fútbol y juegos para niños.</p  class="descripcion-p1"> <p>Amplias y cómodas cabañas de 2 a 6 personas. Las mismas cuentan con detalles de confort</p clas="descripcion-p2"> <p>y categoría contando con todos los servicios necesarios para que su estadia sea inolvidable.</p class="descripcion-p3" > <p><h2 class="Equipamiento">Equipamiento:</h2> todas las cabañas cuentan con baño privado (completo), <h6>cocina totalmente equipada, microondas, heladeras, ventiladores, aire acondicionado (frio-calor), mantas y cobertores. TV por cable, internet Wifi y parrillas.</h6> Deck privado y cocheras individuales con media sombra dentro del predio.</p descripcion-p4> <p> DISCULPEN LAS MOLESTIAS, NO ACEPTAMOS MASCOTAS</p clas="descripcion-p5">'
